58752203ddc278a82d961b5218294ef3d34d6e5e
VanyaMartin <ezirealius@gmail.com>
2025-06-21 06:27:19 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,18 +9,31 @@ export default async function handler(req, res) {
   const session = body.session || {};
 
   const response = {
-    version: body.version || "1.0",
+    version: "1.0",
     session: {
       session_id: session.session_id || "default",
       message_id: session.message_id || 0,
       user_id: session.user_id || "anonymous_user"
     },
     response: {
-      text: "YAHAHAHA, я жив и ору прямо из Vercel!",
-      end_session: false
+      text: " ",
+      tts: " ",
+      end_session: false,
+      directives: [
+        {
+          type: "AudioPlayer.Play",
+          audio_item: {
+            stream: {
+              url: "https://vanyamartin.github.io/Neverlove.mp3",
+              offset_ms: 0
+            }
+          }
+        }
+      ]
     }
   };
 
   res.setHeader("Content-Type", "application/json");
   res.status(200).json(response);
 }
+
