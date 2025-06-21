bf8453ad4c932861a99037a13261454f9cee2dfe
VanyaMartin <ezirealius@gmail.com>
2025-06-21 05:56:08 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
 export default async function handler(req, res) {
-  const body = req.body;
+  const body = req.body || {}; // страховка
 
   const response = {
-    version: body.version,
-    session: body.session,
+    version: body.version || "1.0",
+    session: body.session || { session_id: "default", message_id: 0 },
     response: {
       text: "YAHAHAHA, я жив и ору прямо из Vercel!",
       end_session: false
